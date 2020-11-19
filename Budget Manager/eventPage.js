97b19fae05670f5f2768754ccee7b4e9d0ad94e4
Aparajita Paul <57225563+Tuntai@users.noreply.github.com>
2020-11-19 21:25:01 +0530

    Add files via upload

---
+++ Budget Manager/eventPage.js
@@ -0,0 +1,43 @@
+function isInt(value) {
+	return !isNaN(value) &&
+			parseInt(Number(value)) == value &&
+			!isNaN(parseInt(value, 10));
+}
+
+
+var contextMenuItem = {
+	"id": "spendMoney",
+	"title":"SpendMoney",
+	"contexts":["selection"]
+};
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if(clickData.menuItemId == "spendMoney" && clickData.selectionText){
+		if(isInt(clickData.selectionText)){
+			chrome.storage.sync.get(['total','limit'], function(budget){
+				var newTotal = 0;
+				if(budget.total){
+					newTotal+=parseInt(budget.total);
+				}
+				newTotal+=parseInt(clickData.selectionText);
+				chrome.storage.sync.set({'total':newTotal}, function(){
+					if(newTotal >=budget.limit){
+						var notifOptions = {
+						type: 'basic',
+						iconUrl: 'icon48.png',
+						title: 'Limit Reached!',
+						message: "Oops! You reached your limit!!!"
+					};
+					chrome.notifications.create('limitNotif', notifOptions);
+					}
+				});
+			});
+		}
+	}
+
+});
+
+chrome.storage.onChanged.addListener(function(changes,storageName){
+	chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
+});
